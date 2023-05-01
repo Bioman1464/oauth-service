2d66f84403f394365ff05ed5071e2e025127ff54
Bioman1464 <danilgap1464@gmail.com>
2023-05-02 00:33:32 +0300

    feat(domain/user): allow first name & last name be empty

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -4,8 +4,8 @@ import "github.com/gofrs/uuid"
 
 type User struct {
 	ID        uuid.UUID `json:"id"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
+	FirstName string    `json:"first_name,omitempty"`
+	LastName  string    `json:"last_name,omitempty"`
 	Email     string    `json:"email"`
 	password  string
 }
