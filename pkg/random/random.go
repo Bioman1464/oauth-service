051b620adda0741602033fb5e8830c902db74fbd
Bioman1464 <danilgap1464@gmail.com>
2023-05-08 23:59:31 +0300

    feat(pkg): add random string generator

---
+++ pkg/random/random.go
@@ -0,0 +1,14 @@
+package random
+
+import "math/rand"
+
+func GenerateRandomString(n int) string {
+	const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = alphabet[rand.Intn(len(alphabet))]
+	}
+
+	return string(b)
+}
