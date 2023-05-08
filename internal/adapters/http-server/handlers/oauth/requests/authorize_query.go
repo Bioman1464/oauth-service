c3ecb14fddcdae4d73775b16a192f7b6e09f27b4
Bioman1464 <danilgap1464@gmail.com>
2023-05-09 00:02:10 +0300

    feat(handlers/oauth): update authorize query validation of empty required params

--- internal/adapters/http-server/handlers/oauth/requests/authorize_query.go
+++ internal/adapters/http-server/handlers/oauth/requests/authorize_query.go
@@ -1,16 +1,19 @@
 package requests
 
 import (
+	"errors"
+
 	"github.com/gin-gonic/gin"
 
-	"auth-service/internal/adapters/http-server/handlers/oauth/errors"
+	oauthErr "auth-service/internal/adapters/http-server/handlers/oauth/errors"
 )
 
 type AuthorizeQueryRequest struct {
-	ClientID    string `json:"client_id" form:"client_id"`
-	RedirectURL string `json:"redirect_url" form:"redirect_url"`
-	Scopes      string `json:"scopes" form:"scopes"`
-	State       string `json:"state" form:"state"`
+	ResponseType string `json:"response_type" form:"response_type"`
+	ClientID     string `json:"client_id" form:"client_id"`
+	RedirectURL  string `json:"redirect_url" form:"redirect_url"`
+	Scope        string `json:"scope" form:"scope"`
+	State        string `json:"state" form:"state"`
 }
 
 func (r *AuthorizeQueryRequest) Parse(ctx *gin.Context) error {
@@ -22,8 +25,20 @@ func (r *AuthorizeQueryRequest) Parse(ctx *gin.Context) error {
 }
 
 func (r *AuthorizeQueryRequest) Validate() error {
-	if r.RedirectURL == "" || r.Scopes == "" || r.ClientID == "" {
-		return errors.ErrInvalidRequest
+	if r.RedirectURL == "" {
+		return errors.New("redirect_url is not passed")
+	}
+
+	if r.ClientID == "" {
+		return errors.New("client_id is not passed")
+	}
+
+	if r.ResponseType == "" {
+		return errors.New("response type is not passed")
+	}
+
+	if r.ResponseType != "code" {
+		return oauthErr.ErrUnsupportedResponseType
 	}
 
 	return nil
