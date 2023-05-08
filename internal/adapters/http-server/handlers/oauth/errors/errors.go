36409a59d6caa1ed29f5e33f50d706173354e429
Bioman1464 <danilgap1464@gmail.com>
2023-05-09 00:00:41 +0300

    feat(handlers/oauth): add default errors description & handled errors check

--- internal/adapters/http-server/handlers/oauth/errors/errors.go
+++ internal/adapters/http-server/handlers/oauth/errors/errors.go
@@ -1,7 +1,39 @@
 package errors
 
-import "errors"
+import (
+	"errors"
+)
 
 var (
-	ErrInvalidRequest = errors.New("invalid request")
+	ErrInvalidRequest          = errors.New("invalid_request")
+	ErrUnsupportedResponseType = errors.New("unsupported_response_type")
+	ErrInvalidScope            = errors.New("invalid_scope")
+	ErrUnauthorizedClient      = errors.New("unauthorized_client")
+	ErrServerError             = errors.New("server_error")
 )
+
+var descriptions = map[error]string{
+	ErrInvalidRequest:          "request missing one or more required parameters",
+	ErrUnsupportedResponseType: "passed response type is unsupported",
+	ErrInvalidScope:            "one or more passed scopes are unsupported",
+	ErrUnauthorizedClient:      "client unauthorized to perform action",
+	ErrServerError:             "unexpected condition encountered",
+}
+
+var errorsMap = map[error]struct{}{
+	ErrInvalidScope:            {},
+	ErrUnsupportedResponseType: {},
+	ErrInvalidScope:            {},
+	ErrUnauthorizedClient:      {},
+	ErrServerError:             {},
+}
+
+func GetDescription(err error) string {
+	v, _ := descriptions[err]
+	return v
+}
+
+func IsHandledError(err error) bool {
+	_, ok := errorsMap[err]
+	return ok
+}
